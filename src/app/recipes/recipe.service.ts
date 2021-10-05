413841543649a97384e9d4e42e86ad91d16d3577
Michał Skoczek <mwskoczek@gmail.com>
2021-10-05 12:49:48 +0200

    Finished http section

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,35 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'a test recipe',
-      'this is a simply recipe',
-      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
-      [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
-    ),
-    new Recipe(
-      'a second test recipe',
-      'this is a simply second recipe',
-      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
-      [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
-    ),
-    new Recipe(
-      'a third test recipe',
-      'this is a simply third recipe',
-      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
-      [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'a test recipe',
+  //     'this is a simply recipe',
+  //     'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
+  //     [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
+  //   ),
+  //   new Recipe(
+  //     'a second test recipe',
+  //     'this is a simply second recipe',
+  //     'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
+  //     [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
+  //   ),
+  //   new Recipe(
+  //     'a third test recipe',
+  //     'this is a simply third recipe',
+  //     'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80',
+  //     [new Ingredient('meat', 1), new Ingredient('tomatoes', 3)]
+  //   ),
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
